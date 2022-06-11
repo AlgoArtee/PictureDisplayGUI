02456a466d79492cbdc0cea796ed1486fdd7c9ab
AlgoArtee <laura.hodrea@gmail.com>
2022-06-11 03:32:18 +0200

    Creating panel and jtable

--- src/catshop/CatShopItems.java
+++ src/catshop/CatShopItems.java
@@ -2,11 +2,49 @@ package catshop;
 
 import javax.swing.*;
 
-public class CatShopItems {
-    private JTable itemDisplay;
-    private JPanel CatShop;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-    private void createUIComponents() {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
+public class CatShopItems extends JFrame {
+
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Throwable e){
+            e.printStackTrace();
+        }
+
+
+        EventQueue.invokeLater(() -> {
+            try {
+                CatShopItems frame = new CatShopItems();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
+
+    /**
+     * Create the frame.
+     */
+    public CatShopItems() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+    }
+
 }
