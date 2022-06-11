0625ea6f49fa1007ae79a436affe38f1ad18e905
AlgoArtee <laura.hodrea@gmail.com>
2022-06-11 02:01:09 +0200

    Creating panel and jtable

---
+++ src/catshop/CatShopItems.java
@@ -0,0 +1,12 @@
+package catshop;
+
+import javax.swing.*;
+
+public class CatShopItems {
+    private JTable itemDisplay;
+    private JPanel CatShop;
+
+    private void createUIComponents() {
+
+    }
+}
