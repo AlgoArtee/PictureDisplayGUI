568c22aeb0100e6ee287f34aa0141941601b38e3
AlgoArtee <laura.hodrea@gmail.com>
2022-06-16 00:54:26 +0200

    Added Data and Pictures to Table

--- src/catshop/CatShopItems.java
+++ src/catshop/CatShopItems.java
@@ -1,16 +1,18 @@
 package catshop;
 
 import domain.Shop;
+import valueobjects.Item;
 
 import javax.swing.*;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.*;
 import java.io.IOException;
+import java.util.Map;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 public class CatShopItems extends JFrame {
@@ -47,7 +49,7 @@ public class CatShopItems extends JFrame {
      */
     public CatShopItems() throws IOException, ClassNotFoundException {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
+        setBounds(100, 100, 850, 600);
 
         contentPanel = new JPanel();
         contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -55,7 +57,7 @@ public class CatShopItems extends JFrame {
         setContentPane(contentPanel);
 
         itemDisplay = new JTable();
-        //contentPanel.add(itemDisplay, BorderLayout.CENTER);
+        itemDisplay.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
         loadShopItems();
 
@@ -67,7 +69,7 @@ public class CatShopItems extends JFrame {
 
     private void loadShopItems() throws IOException, ClassNotFoundException {
 
-        shop.getInventory();
+        Map<Integer, Item> itemList = shop.getInventory();
 
         DefaultTableModel dtf = new DefaultTableModel(){
             @Override
@@ -85,8 +87,40 @@ public class CatShopItems extends JFrame {
         dtf.addColumn("In Stock");
         dtf.addColumn("Bulk Size");
 
+        for (Map.Entry<Integer, Item> entry : itemList.entrySet()){
+
+            dtf.addRow(new Object[] {
+                    entry.getKey(),
+                    entry.getValue().getItemName(),
+                    entry.getValue().getPic(),
+                    entry.getValue().getIsAvailable(),
+                    entry.getValue().getItemPrice(),
+                    entry.getValue().getDescription(),
+                    entry.getValue().getAmountInStock(),
+                    entry.getValue().getBulkSize()
+            });
+
+        }
+
+
+
         itemDisplay.setModel(dtf);
         //itemDisplay.setRowHeight(0,100);
-        itemDisplay.setRowHeight(50);
+        itemDisplay.setRowHeight(70);
+        itemDisplay.getTableHeader().setReorderingAllowed(false);
+        itemDisplay.getColumnModel().getColumn(2).setCellRenderer(new ImageRenderer());
     }
+
+    private class ImageRenderer extends DefaultTableCellRenderer{
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                                                       boolean hasFocus, int row, int colum){
+            String photoName = value.toString();
+            ImageIcon imageIcon = new ImageIcon(new ImageIcon("src/itemresources/"+photoName).
+                    getImage().getScaledInstance(60,60, Image.SCALE_DEFAULT));
+            return new JLabel(imageIcon);
+        }
+    }
+
 }
